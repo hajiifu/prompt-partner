a524bba71db3c219c3bf8a7be3739d00464afdcc
hajiifu <kaz428mrn@gmail.com>
2023-10-16 01:29:35 +0900

    chore: add noindex config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // head要素の設定
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "ja",
+      },
+      meta: [
+        // noindexの設定
+        { name: "robots", content: "noindex" },
+      ],
+    },
+  },
   devtools: { enabled: true },
   ssr: false,
   modules: ["@nuxtjs/tailwindcss"],
